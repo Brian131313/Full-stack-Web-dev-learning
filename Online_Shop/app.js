fa7bed9277460c3d3542f9fc4ee94487d75e9ae2
Brian131313 <brian131313lx@gmail.com>
2022-08-04 12:24:01 +0800

    add singup & login routes and controller(empry, temporary), and put authRoutes middleware inside app.js

--- Online_Shop/app.js
+++ Online_Shop/app.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
+//here import express package and define it as app. 
+//so, it's pretty clear here, app equals express package.
+
+const authRoutes = require('./routes/auth-routes')
+//here import authRoutes from other file with a path.
+
+app.use(authRoutes);
+//'use' is a built-in method form express app object that allows us to add a middleware.
+
 
 app.listen(3000);
