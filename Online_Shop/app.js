b77536fbe55ea555f0af8cf66c5c774dbb8ea4af
Brian131313 <brian131313lx@gmail.com>
2022-08-04 14:45:49 +0800

    adding EJS & first views

--- Online_Shop/app.js
+++ Online_Shop/app.js
@@ -1,3 +1,6 @@
+const path = require('path');
+//path package is built into Node.js already.
+
 const express = require('express');
 const app = express();
 //here import express package and define it as app. 
@@ -5,6 +8,12 @@ const app = express();
 
 const authRoutes = require('./routes/auth-routes')
 //here import authRoutes from other file with a path.
+
+app.set('views engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+// here set view engine and path. 
+// join is a method form path package.
+// __dirname is global method.
 
 app.use(authRoutes);
 //'use' is a built-in method form express app object that allows us to add a middleware.
