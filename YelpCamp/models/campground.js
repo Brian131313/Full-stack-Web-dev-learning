c124de102d5ce28b759c69b13f72d3e6d33c82be
Brian131313 <brian131313lx@gmail.com>
2022-08-22 17:17:01 +0800

    review

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -1,12 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-  title: { type: String, required: true },
-  image: { type: String, required: true },
-  price: { type: Number, required: true, min: 0 },
-  description: { type: String, required: true },
-  location: { type: String, required: true },
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
-module.exports = mongoose.model("Campground", CampgroundSchema);
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
